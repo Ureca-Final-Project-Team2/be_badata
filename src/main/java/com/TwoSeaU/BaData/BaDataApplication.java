a3a61cef1371b7723de6ac3a33b78a78a6352f2c
Jinu Yi <dionisos198@naver.com>
2025-07-01 17:08:24 +0900

    setting: 프로젝트 초기 세팅

---
+++ src/main/java/com/TwoSeaU/BaData/BaDataApplication.java
@@ -0,0 +1,13 @@
+package com.TwoSeaU.BaData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TwoSeaU/BaData/BaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TwoSeaU.BaData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
