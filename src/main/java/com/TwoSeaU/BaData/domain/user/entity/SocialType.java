1a41fccce0557f257601ffc5c8ab592de40df30c
Jinu Yi <dionisos198@naver.com>
2025-07-08 11:06:56 +0900

    Feat: [BADA-68] 필요한 User 칼럼 추가 #29

---
+++ src/main/java/com/TwoSeaU/BaData/domain/user/entity/SocialType.java
@@ -0,0 +1,27 @@
+package com.TwoSeaU.BaData.domain.user.entity;
+
+import com.TwoSeaU.BaData.domain.auth.exception.AuthException;
+import com.TwoSeaU.BaData.global.response.GeneralException;
+import java.util.Arrays;
+import lombok.Getter;
+
+@Getter
+public enum SocialType {
+
+    KAKAO("kakao");
+
+    private final String value;
+
+    SocialType(String value) {
+        this.value = value;
+    }
+
+    public static SocialType from(String value) {
+
+        return Arrays.stream(values())
+                .filter(s -> s.getValue().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(()->new GeneralException(AuthException.NOT_SUPPORTED_SOCIAL_TYPE));
+    }
+}
+
