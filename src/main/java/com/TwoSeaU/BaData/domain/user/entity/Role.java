e1dfa7f7de1c6d72c7417414b66127237303cecf
Jinu Yi <dionisos198@naver.com>
2025-07-08 11:07:40 +0900

    Feat: [BADA-68] 필요한 User 역할 추가 #29

---
+++ src/main/java/com/TwoSeaU/BaData/domain/user/entity/Role.java
@@ -0,0 +1,5 @@
+package com.TwoSeaU.BaData.domain.user.entity;
+
+public enum Role {
+    ADMIN,GENERAL
+}
--- src/main/java/com/TwoSeaU/BaData/domain/user/repository/UserRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.TwoSeaU.BaData.domain.user.repository;
 
 import com.TwoSeaU.BaData.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
 
+    Optional<User> findByUsername(String username);
 }
