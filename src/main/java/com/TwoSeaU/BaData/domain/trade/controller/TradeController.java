a2bddc81b307ac5608e4ae150c5ea4223561b945
marineAqu <wii021004@naver.com>
2025-07-08 14:47:09 +0900

    Feat: [BADA-70] 게시글 검색 API 개발 #31

---
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/controller/TradeController.java
@@ -0,0 +1,34 @@
+package com.TwoSeaU.BaData.domain.trade.controller;
+
+import com.TwoSeaU.BaData.domain.trade.dto.response.PostsResponse;
+import com.TwoSeaU.BaData.domain.trade.service.PostService;
+import com.TwoSeaU.BaData.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/trades")
+public class TradeController {
+    private final PostService postService;
+
+    @GetMapping("/posts")
+    public ResponseEntity<ApiResponse<PostsResponse>> getPosts(@RequestParam(required = false) String query) {
+        if (query != null && !query.isEmpty()) {
+            return ResponseEntity.ok().body(ApiResponse.success(postService.searchPosts(query)));
+        }
+
+        return ResponseEntity.ok().body(ApiResponse.success(postService.findAllPosts()));
+    }
+
+    @GetMapping("/posts/{userId}")
+    public ResponseEntity<ApiResponse<PostsResponse>> getPostsByUserId(@PathVariable Long userId) {
+        return ResponseEntity.ok().body(ApiResponse.success(postService.findPostsByUserId(userId)));
+    }
+
+    @GetMapping("/posts/deadline")
+    public ResponseEntity<ApiResponse<PostsResponse>> getPostsByDeadLine() {
+        return ResponseEntity.ok().body(ApiResponse.success(postService.findPostsDeadLine()));
+    }
+}
