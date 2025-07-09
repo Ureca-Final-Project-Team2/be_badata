39fa44ee5b3d1a94f41933094fcd917a8dd3283c
marineAqu <wii021004@naver.com>
2025-07-09 09:57:18 +0900

    Feat: [BADA-83] PostLikesRepository 생성 #42

---
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostLikesRepository.java
@@ -0,0 +1,7 @@
+package com.TwoSeaU.BaData.domain.trade.repository;
+
+import com.TwoSeaU.BaData.domain.trade.entity.PostLikes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostLikesRepository extends JpaRepository<PostLikes, Long> {
+}
