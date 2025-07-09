0264e72fdf55c1ff9515305b90c591f0efa6eee9
marineAqu <wii021004@naver.com>
2025-07-09 09:58:47 +0900

    Feat: [BADA-83] ReportRepository 생성 #42

---
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/ReportRepository.java
@@ -0,0 +1,8 @@
+package com.TwoSeaU.BaData.domain.trade.repository;
+
+import com.TwoSeaU.BaData.domain.trade.entity.Report;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReportRepository extends JpaRepository<Report, Long> {
+
+}
