86f6555edb1cc2ed1cb0a5575cab5740d2e7dc39
Jinu Yi <dionisos198@naver.com>
2025-07-10 10:31:43 +0900

    Refactor: [BADA-49] 예약 ENUM 띄어쓰기 명시 #6

--- src/main/java/com/TwoSeaU/BaData/domain/rental/enums/ReservationStatus.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/enums/ReservationStatus.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public enum ReservationStatus {
 
-    PENDING("수령 전"),BURROWING("대여 중"),COMPLETE("완료");
+    PENDING("수령 전"), BURROWING("대여 중"), COMPLETE("완료");
 
     private final String value;
     ReservationStatus(String value) {
