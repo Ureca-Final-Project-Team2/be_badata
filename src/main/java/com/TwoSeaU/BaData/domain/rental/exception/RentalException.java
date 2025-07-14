fdbd779322def4fc89b1e97f7944fb2665a5470d
Jinu Yi <dionisos198@naver.com>
2025-07-14 12:30:31 +0900

    Refactor: [BADA-122] 예외처리 시 마침표 포함 #95

--- src/main/java/com/TwoSeaU/BaData/domain/rental/exception/RentalException.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/exception/RentalException.java
@@ -10,9 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum RentalException implements BaseException {
 
     ALREADY_RENTAL_EXIST_SAME_PERIOD(HttpStatus.BAD_REQUEST, 4002, "이미 해당 기간에 예약이 존재합니다."),
-    RESERVATION_NOT_FOUND(HttpStatus.BAD_REQUEST, 4003, "해당 예약을 찾을 수 없습니다"),
-    CANT_CANCEL_ALREADY_RENTAL(HttpStatus.BAD_REQUEST, 4004, "이미 빌리거나 이미 사용한 경우 예약을 취소할 수 없습니다"),
-    CANT_CANCEL_RESERVED_USERS(HttpStatus.BAD_REQUEST, 4005, "예약을 진행한 당사자만 예약을 취소할 수 있습니다");
+    RESERVATION_NOT_FOUND(HttpStatus.BAD_REQUEST, 4003, "해당 예약을 찾을 수 없습니다."),
+    CANT_CANCEL_ALREADY_RENTAL(HttpStatus.BAD_REQUEST, 4004, "이미 빌리거나 이미 사용한 경우 예약을 취소할 수 없습니다."),
+    CANT_CANCEL_RESERVED_USERS(HttpStatus.BAD_REQUEST, 4005, "예약을 진행한 당사자만 예약을 취소할 수 있습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
--- src/main/java/com/TwoSeaU/BaData/domain/store/exception/StoreException.java
+++ src/main/java/com/TwoSeaU/BaData/domain/store/exception/StoreException.java
@@ -9,8 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum StoreException implements BaseException {
 
-    CANT_FIND_STORE(HttpStatus.NOT_FOUND, 4001, "해당 가맹점을 찾을 수 없습니다"),
-    CANT_FIND_STORE_DEVICE(HttpStatus.NOT_FOUND,4002,"해당 가맹점이 보유한 기기를 찾을 수 없습니다");
+    CANT_FIND_STORE(HttpStatus.NOT_FOUND, 4001, "해당 가맹점을 찾을 수 없습니다."),
+    CANT_FIND_STORE_DEVICE(HttpStatus.NOT_FOUND,4002,"해당 가맹점이 보유한 기기를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final int code;
