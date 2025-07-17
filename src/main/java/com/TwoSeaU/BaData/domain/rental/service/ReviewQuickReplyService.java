a01b672e1f4812cd45b29bf8b62e7b94f3ba69dc
Jinu Yi <dionisos198@naver.com>
2025-07-17 15:46:03 +0900

    Refactor: [BADA-147] readOnly 적용 #127

--- src/main/java/com/TwoSeaU/BaData/domain/rental/service/ReviewQuickReplyService.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/service/ReviewQuickReplyService.java
@@ -6,9 +6,11 @@ import com.TwoSeaU.BaData.domain.rental.repository.QuickReplyRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ReviewQuickReplyService {
 
     private final QuickReplyRepository quickReplyRepository;
