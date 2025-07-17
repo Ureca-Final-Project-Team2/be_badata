0fc4829b3acd7212931e865b44080994de8488e1
Jinu Yi <dionisos198@naver.com>
2025-07-17 22:35:20 +0900

    Refactor: [BADA-148] storeDevice 와 store 사이에 연관 검증 #129

--- src/main/java/com/TwoSeaU/BaData/domain/rental/exception/RentalException.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/exception/RentalException.java
@@ -15,7 +15,8 @@ public enum RentalException implements BaseException {
     CANT_CANCEL_RESERVED_USERS(HttpStatus.BAD_REQUEST, 4005, "예약을 진행한 당사자만 예약을 취소할 수 있습니다."),
     CANT_RESTOCK_WHEN_AVAILABLE_COUNT(HttpStatus.BAD_REQUEST, 4006, "예약 가능한 상황일 때는 재입고를 신청할 수 없습니다"),
     CANT_RESTOCK_MORE_THAN_COUNT(HttpStatus.BAD_REQUEST, 4007, "재입고 알림 대수는 가맹점이 소유한 기기 이상으로 할 수 없습니다"),
-    CANT_END_DATE_BEFORE_THAN_START_DATE(HttpStatus.BAD_REQUEST, 4008, "예약 재입고 시 시작 날짜는 종료 날짜보다 먼저 와야 합니다");
+    CANT_END_DATE_BEFORE_THAN_START_DATE(HttpStatus.BAD_REQUEST, 4008, "예약 재입고 시 시작 날짜는 종료 날짜보다 먼저 와야 합니다"),
+    DONT_MATCH_STORE_DEVICE_STORE(HttpStatus.BAD_REQUEST,4009,"장치를 소유한 가맹점과 가맹점이 일치하지 않습니다");
 
     private final HttpStatus httpStatus;
     private final int code;
