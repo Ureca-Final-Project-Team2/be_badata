e372054fa1ed25b15e522bc6e2074b3b38768cc6
marineAqu <wii021004@naver.com>
2025-07-17 18:35:51 +0900

    fix: [BADA-141] Validation Exception 추가 #121

--- src/main/java/com/TwoSeaU/BaData/domain/trade/dto/request/SaveDataPostRequest.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/dto/request/SaveDataPostRequest.java
@@ -1,21 +1,35 @@
 package com.TwoSeaU.BaData.domain.trade.dto.request;
 
 import com.TwoSeaU.BaData.domain.trade.enums.MobileCarrier;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class SaveDataPostRequest {
+    @NotNull(message = "제목은 필수 작성입니다.")
     private String title;
+
+    @NotNull(message = "통신사는 필수 선택입니다.")
     private MobileCarrier mobileCarrier;
+
+    @NotNull(message = "유효기간은 필수 입력입니다.")
     private LocalDateTime deadLine;
+
+    @NotNull(message = "데이터 용량은 필수 입력입니다.")
     private Integer capacity;
+
+    @NotNull(message = "가격은 필수 작성입니다.")
     private Integer price;
+
+    @NotNull(message = "상세 설명은 필수 작성입니다.")
     private String comment;
+
+    @NotNull(message = "쿠폰 이미지 등록은 필수입니다.")
     private MultipartFile file;
 }
