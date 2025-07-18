f5bd7a51db3686d317c01bb1369eab1c573094ea
marineAqu <wii021004@naver.com>
2025-07-18 14:36:17 +0900

    Feat: [BADA-157] 유효성 검사 추가 #145

--- src/main/java/com/TwoSeaU/BaData/domain/trade/dto/request/UpdatePostRequest.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/dto/request/UpdatePostRequest.java
@@ -1,12 +1,16 @@
 package com.TwoSeaU.BaData.domain.trade.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class UpdatePostRequest {
+    @NotNull(message = "상세 설명은 비워둘 수 없습니다.")
     private String comment;
+
+    @NotNull(message = "가격은 비워둘 수 없습니다.")
     private Integer price;
 }
