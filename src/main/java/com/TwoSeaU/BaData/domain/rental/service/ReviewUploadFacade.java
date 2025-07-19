c918ee8effcb10f4237e7bccedf6b80cb468572b
Jinu Yi <dionisos198@naver.com>
2025-07-19 18:59:23 +0900

    Refactor: [BADA-168] Review 이미지 검증 로직 구체화 #155

--- src/main/java/com/TwoSeaU/BaData/domain/rental/service/ReviewUploadFacade.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/service/ReviewUploadFacade.java
@@ -5,6 +5,7 @@ import com.TwoSeaU.BaData.domain.rental.dto.request.UpdateReviewRequest;
 import com.TwoSeaU.BaData.global.s3.S3ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -16,7 +17,7 @@ public class ReviewUploadFacade {
 
     public Long uploadReviewWithImage(final CreateReviewRequest createReviewRequest, final String username){
 
-        final String imageUrl = createReviewRequest.getFile()==null ? null : s3ImageService.saveImage(createReviewRequest.getFile(),
+        final String imageUrl = cantGetImageUrl(createReviewRequest.getFile()) ? null : s3ImageService.saveImage(createReviewRequest.getFile(),
                                                                                                 reviewDirectory,
                                                                                                  createReviewRequest.getFile().getOriginalFilename());
 
@@ -25,11 +26,20 @@ public class ReviewUploadFacade {
 
     public Long changeReviewWithImage(final Long reviewId, final UpdateReviewRequest updateReviewRequest, final String username){
 
-        final String imageUrl = updateReviewRequest.getFile()==null ? null : s3ImageService.saveImage(updateReviewRequest.getFile(),
+        final String imageUrl = cantGetImageUrl(updateReviewRequest.getFile()) ? null : s3ImageService.saveImage(updateReviewRequest.getFile(),
                 reviewDirectory,
                 updateReviewRequest.getFile().getOriginalFilename());
 
         return reviewService.changeReview(reviewId, updateReviewRequest, username, imageUrl);
+    }
+
+    final boolean cantGetImageUrl(final MultipartFile file){
+
+        if (file ==null || file.isEmpty()){
+            return true;
+        }
+
+        return false;
     }
 
 }
