f29f87d843d23bf56799e0a0b8f59b6f633ae28a
marineAqu <wii021004@naver.com>
2025-07-21 14:23:45 +0900

    Test: [BADA-174] Data Post 등록 테스트 코드 작성 #164

--- src/test/java/com/TwoSeaU/BaData/domain/trade/service/PostServiceTest.java
+++ src/test/java/com/TwoSeaU/BaData/domain/trade/service/PostServiceTest.java
@@ -2,11 +2,15 @@ package com.TwoSeaU.BaData.domain.trade.service;
 
 import com.TwoSeaU.BaData.domain.trade.dto.ELAResult;
 import com.TwoSeaU.BaData.domain.trade.dto.SuspiciousRegion;
+import com.TwoSeaU.BaData.domain.trade.dto.request.SaveDataPostRequest;
 import com.TwoSeaU.BaData.domain.trade.dto.request.SaveGifticonPostRequest;
 import com.TwoSeaU.BaData.domain.trade.dto.response.SavePostResponse;
+import com.TwoSeaU.BaData.domain.trade.entity.Data;
 import com.TwoSeaU.BaData.domain.trade.entity.Gifticon;
 import com.TwoSeaU.BaData.domain.trade.entity.GifticonCategory;
+import com.TwoSeaU.BaData.domain.trade.enums.MobileCarrier;
 import com.TwoSeaU.BaData.domain.trade.exception.TradeException;
+import com.TwoSeaU.BaData.domain.trade.repository.DataRepository;
 import com.TwoSeaU.BaData.domain.trade.repository.GifticonCategoryRepository;
 import com.TwoSeaU.BaData.domain.trade.repository.GifticonRepository;
 import com.TwoSeaU.BaData.domain.user.entity.User;
@@ -54,6 +58,8 @@ class PostServiceTest {
     @Mock
     GifticonCategoryRepository gifticonCategoryRepository;
     @Mock
+    DataRepository dataRepository;
+    @Mock
     ELAService elaService;
     @Mock
     S3ImageService s3ImageService;
@@ -61,6 +67,7 @@ class PostServiceTest {
     private static final String USERNAME = "testUser";
     private static final String IMAGE_URL = "https://s3.amazonaws.com/trades/gifticon/test.jpg";
     private static final String CATEGORY_NAME = "OTT/뮤직";
+
 
     @Nested
     @DisplayName("createGifticonPost 메서드는")
@@ -175,6 +182,56 @@ class PostServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("createDataPost 메서드는")
+    class Describe_createDataPost {
+
+        @Nested
+        @DisplayName("정상 흐름(해피 케이스)에서는")
+        class Context_with_success {
+
+            @Test
+            @DisplayName("데이터 게시물을 생성한다.")
+            void it_creates_gifticon_post() {
+                // given
+                User user = createTestUser();
+                SaveDataPostRequest request = createSaveDataPostRequest();
+
+                given(userRepository.findByUsername(USERNAME)).willReturn(Optional.of(user));
+                Data saveData = mock(Data.class);
+                given(saveData.getId()).willReturn(1L);
+                given(dataRepository.save(any(Data.class))).willReturn(saveData);
+
+                // when
+                SavePostResponse response = postService.createDataPost(request, USERNAME);
+
+                // then
+                assertThat(response.getPostId()).isEqualTo(1L);
+            }
+        }
+
+        @Nested
+        @DisplayName("예외 상황에서는")
+        class Context_with_failure {
+            @Test
+            @DisplayName("사용자를 찾을 수 없으면 USER_NOT_FOUND 예외를 던진다.")
+            void it_throws_exception_when_user_not_found() {
+                // given
+                String username = "notFoundUser";
+                SaveDataPostRequest request = createSaveDataPostRequest();
+                given(userRepository.findByUsername(username)).willReturn(Optional.empty());
+
+                // when & then
+                GeneralException ex = assertThrows(GeneralException.class, () -> {
+                    postService.createDataPost(request, username);
+                });
+
+                assertThat(ex.getBaseException().getMessage()).isEqualTo(
+                        UserException.USER_NOT_FOUND.getMessage());
+            }
+        }
+    }
+
     private GifticonCategory createTestGifticonCategory() {
         return mock(GifticonCategory.class);
     }
@@ -190,6 +247,17 @@ class PostServiceTest {
         given(request.getCouponNumber()).willReturn("1234567890");
         given(request.getPartner()).willReturn("스타벅스");
         given(request.getFile()).willReturn(file);
+        return request;
+    }
+
+    private SaveDataPostRequest createSaveDataPostRequest() {
+        SaveDataPostRequest request = mock(SaveDataPostRequest.class);
+        given(request.getTitle()).willReturn("테스트 데이터");
+        given(request.getComment()).willReturn("테스트 설명");
+        given(request.getPrice()).willReturn(5000);
+        given(request.getDeadLine()).willReturn(LocalDate.now().plusDays(7));
+        given(request.getMobileCarrier()).willReturn(MobileCarrier.SKT);
+        given(request.getCapacity()).willReturn(64);
         return request;
     }
 
