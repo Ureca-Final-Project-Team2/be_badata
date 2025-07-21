fe1698d7db0b837aac30a0ef307b7ec94b7f4b63
choyunju <yoonj1860@naver.com>
2025-07-21 10:37:31 +0900

    Feat: [BADA-166] 사용자 공유기 대여 내역 조회 API

--- src/main/java/com/TwoSeaU/BaData/domain/rental/repository/ReservationQueryRepositoryImpl.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/repository/ReservationQueryRepositoryImpl.java
@@ -21,9 +21,9 @@ public class ReservationQueryRepositoryImpl implements ReservationQueryRepositor
 	private final JPAQueryFactory queryFactory;
 
 	public CursorPageResponse<GetRentalResponse> getAllRentalsByCursor(final Long cursor, final int size, final Long userId) {
-		QReservation qReservation = QReservation.reservation;
-		QStore qStore = QStore.store;
-		BooleanBuilder where = new BooleanBuilder();
+		final QReservation qReservation = QReservation.reservation;
+		final QStore qStore = QStore.store;
+		final BooleanBuilder where = new BooleanBuilder();
 
 		if(cursor != null) {
 			where.and(qReservation.id.lt(cursor));
@@ -31,18 +31,18 @@ public class ReservationQueryRepositoryImpl implements ReservationQueryRepositor
 
 		where.and(qReservation.user.id.eq(userId));
 
-		List<Reservation> fetchedList = queryFactory.selectFrom(qReservation)
+		final List<Reservation> fetchedList = queryFactory.selectFrom(qReservation)
 			.leftJoin(qReservation.store, qStore).fetchJoin()
 			.where(where)
 			.orderBy(qReservation.id.desc())
 			.limit(size + 1)
 			.fetch();
 
-		boolean hasNext = fetchedList.size() > size;
-		List<Reservation> qReservationList = hasNext
+		final boolean hasNext = fetchedList.size() > size;
+		final List<Reservation> qReservationList = hasNext
 			? fetchedList.subList(0, size) : fetchedList;
 
-		List<GetRentalResponse> responseList = qReservationList.stream()
+		final List<GetRentalResponse> responseList = qReservationList.stream()
 			.map(reservation -> {
 				Store store = reservation.getStore();
 				if(store == null) {
@@ -53,7 +53,7 @@ public class ReservationQueryRepositoryImpl implements ReservationQueryRepositor
 			})
 			.toList();
 
-		Long nextCursor = responseList.isEmpty() ? null : responseList.get(responseList.size() - 1).getId();
+		final Long nextCursor = responseList.isEmpty() ? null : responseList.get(responseList.size() - 1).getId();
 
 		return CursorPageResponse.of(responseList, nextCursor, hasNext);
 	}
