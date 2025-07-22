8125e28ce262fd71e58cae88a3cac5ade3e78101
marineAqu <wii021004@naver.com>
2025-07-22 12:44:02 +0900

    Fix: [BADA-197] Entity 자료형 수정에 따른 매개변수 수정 #188

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
@@ -2,12 +2,12 @@ package com.TwoSeaU.BaData.domain.trade.repository;
 
 import com.TwoSeaU.BaData.domain.trade.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
     List<Post> findByIsSoldOrderByCreatedAtDesc(boolean isSold);
     List<Post> findByIsSoldAndSellerIdOrderByCreatedAtDesc(boolean isSold, Long sellerId);
-    List<Post> findByDeadLineBefore(LocalDateTime time);
+    List<Post> findByDeadLineBefore(LocalDate time);
     List<Post> findByTitleContaining(String query);
 }
