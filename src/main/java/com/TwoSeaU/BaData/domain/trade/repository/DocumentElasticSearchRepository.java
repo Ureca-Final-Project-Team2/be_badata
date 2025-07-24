06479d8c6bf2826d3748f2521b4083f7252c1072
marineAqu <wii021004@naver.com>
2025-07-24 14:47:07 +0900

    Fix: [BADA-207] 타입 불일치 해결 #192

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/DocumentElasticSearchRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/DocumentElasticSearchRepository.java
@@ -3,5 +3,5 @@ package com.TwoSeaU.BaData.domain.trade.repository;
 import com.TwoSeaU.BaData.domain.trade.entity.SearchHistoryDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface DocumentElasticSearchRepository extends ElasticsearchRepository<SearchHistoryDocument, Long> {
+public interface DocumentElasticSearchRepository extends ElasticsearchRepository<SearchHistoryDocument, String> {
 }
