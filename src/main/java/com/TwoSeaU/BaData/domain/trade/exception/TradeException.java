6abf2f99a21786b814b3324e1cd77561ce45b090
marineAqu <wii021004@naver.com>
2025-07-25 16:02:59 +0900

    Feat: [BADA-233] 본인 게시글 구매 방지 #228

--- src/main/java/com/TwoSeaU/BaData/domain/trade/exception/TradeException.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/exception/TradeException.java
@@ -31,7 +31,9 @@ public enum TradeException implements BaseException {
     REPORT_COMMENT_REQUIRED(HttpStatus.BAD_REQUEST, 3019, "신고 사유가 기타일 경우 상세 사유를 작성해야 합니다."),
     DELETED_POST_ACCESS_DENIED(HttpStatus.GONE, 3020, "삭제된 게시글에 접근할 수 없습니다."),
     ALREADY_LIKED_POST(HttpStatus.CONFLICT, 3021, "이미 찜한 게시글입니다."),
-    SOLD_POST_DELETE_DENIED(HttpStatus.FORBIDDEN, 3022, "판매 완료된 게시글은 삭제할 수 없습니다."),;
+    SOLD_POST_DELETE_DENIED(HttpStatus.FORBIDDEN, 3022, "판매 완료된 게시글은 삭제할 수 없습니다."),
+    SELF_PAYMENT_DENIED(HttpStatus.FORBIDDEN, 3023, "본인 게시글은 구매할 수 없습니다."),
+    ;
 
     private final HttpStatus httpStatus;
     private final int code;
