affd7d9661a2e59aef1fa7f82e001017784357e8
Jinu Yi <dionisos198@naver.com>
2025-07-27 00:39:44 +0900

    Feat: [BADA-248] if 다음 띄어쓰기 지정 #247

--- src/main/java/com/TwoSeaU/BaData/domain/rental/service/RentalService.java
+++ src/main/java/com/TwoSeaU/BaData/domain/rental/service/RentalService.java
@@ -47,11 +47,11 @@ public class RentalService {
                                                                                     final LocalDateTime rentalEndDate,
                                                                                     final Long storeId){
 
-        if(!storeRepository.existsById(storeId)){
+        if (!storeRepository.existsById(storeId)){
             throw new GeneralException(StoreException.CANT_FIND_STORE);
         }
 
-        if(rentalStartDate == null || rentalEndDate == null){
+        if (rentalStartDate == null || rentalEndDate == null){
             return deviceReservationRepository.findAvailableDevicesByStoreId(storeId).stream()
                     .map(ShowReservationDeviceInfoResponse::from).toList();
         }
