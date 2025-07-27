84da2c023bb5ad957b47c204e96514da11455fb5
marineAqu <wii021004@naver.com>
2025-07-27 11:46:26 +0900

    Fix: [BADA-253] 코인 사용 로직 추가, 결제 금액 검증, 사용 코인 반영 #253

--- src/main/java/com/TwoSeaU/BaData/domain/trade/exception/TradeException.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/exception/TradeException.java
@@ -36,7 +36,8 @@ public enum TradeException implements BaseException {
     NOT_FOUND_GIFTICON_PARTNER(HttpStatus.NOT_FOUND, 3024, "해당 제휴처를 찾을 수 없습니다."),
     EXPIRED_EXPIRATION_DATE(HttpStatus.BAD_REQUEST, 3025, "유효 기간이 지난 기프티콘입니다."),
     CANNOT_READ_FROM_IMAGE(HttpStatus.BAD_REQUEST, 3026, "이미지 파일을 읽어올 수 없습니다."),
-    CANNOT_PARSE_DATE(HttpStatus.BAD_REQUEST, 3027, "날짜 파싱 처리에 실패하였습니다.")
+    CANNOT_PARSE_DATE(HttpStatus.BAD_REQUEST, 3027, "날짜 파싱 처리에 실패하였습니다."),
+    PAYMENT_AMOUNT_MISMATCH(HttpStatus.BAD_REQUEST, 3028, "결제 금액이 게시글 가격과 일치하지 않습니다."),
     ;
 
     private final HttpStatus httpStatus;
--- src/main/java/com/TwoSeaU/BaData/domain/trade/service/PaymentService.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.TwoSeaU.BaData.domain.trade.service;
 
+import com.TwoSeaU.BaData.domain.trade.dto.request.GetMerchantUidRequest;
 import com.TwoSeaU.BaData.domain.trade.dto.response.CreatePaymentResponse;
 import com.TwoSeaU.BaData.domain.trade.dto.response.GetValidatePaymentResponse;
 import com.TwoSeaU.BaData.domain.trade.entity.Payment;
@@ -9,8 +10,11 @@ import com.TwoSeaU.BaData.domain.trade.enums.PaymentStatus;
 import com.TwoSeaU.BaData.domain.trade.exception.TradeException;
 import com.TwoSeaU.BaData.domain.trade.repository.PaymentRepository;
 import com.TwoSeaU.BaData.domain.trade.repository.PostRepository;
+import com.TwoSeaU.BaData.domain.user.entity.CoinHistory;
 import com.TwoSeaU.BaData.domain.user.entity.User;
+import com.TwoSeaU.BaData.domain.user.enums.CoinSource;
 import com.TwoSeaU.BaData.domain.user.exception.UserException;
+import com.TwoSeaU.BaData.domain.user.repository.CoinHistoryRepository;
 import com.TwoSeaU.BaData.domain.user.repository.UserRepository;
 import com.TwoSeaU.BaData.global.response.GeneralException;
 import com.siot.IamportRestClient.IamportClient;
@@ -18,8 +22,8 @@ import com.siot.IamportRestClient.exception.IamportResponseException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
@@ -32,9 +36,10 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final CoinHistoryRepository coinHistoryRepository;
     private final IamportClient iamportClient;
 
-    public CreatePaymentResponse createOrder(final Long postId, final String username) {
+    public CreatePaymentResponse createOrder(final Long postId, final String username, final GetMerchantUidRequest getMerchantUidRequest) {
         final User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new GeneralException(UserException.USER_NOT_FOUND));
 
@@ -53,7 +58,9 @@ public class PaymentService {
             throw new GeneralException(TradeException.SELF_PAYMENT_DENIED);
         }
 
-        final Payment payment = Payment.of(user, post, generateMerchantUid(), PayMethod.CARD, new BigDecimal(post.getPrice()));
+        final Payment payment = Payment.of(user, post, generateMerchantUid(), PayMethod.CARD,
+                post.getPrice(), getMerchantUidRequest.getUseCoin());
+
         paymentRepository.save(payment);
 
         return CreatePaymentResponse.of(payment.getMerchantUid());
@@ -74,18 +81,24 @@ public class PaymentService {
             throw new GeneralException(TradeException.DELETED_POST_ACCESS_DENIED);
         }
 
-        if(post.getIsSold()){
+        if (post.getIsSold()) {
             throw new GeneralException(TradeException.PAYMENT_DUPLICATE);
         }
 
-        if(Objects.equals(post.getSeller().getId(), user.getId())) {
+        if (Objects.equals(post.getSeller().getId(), user.getId())) {
             throw new GeneralException(TradeException.SELF_PAYMENT_DENIED);
+        }
+
+        if (!post.getPrice().equals(iamportClient.paymentByImpUid(impUid).getResponse().getAmount())) {
+            throw new GeneralException(TradeException.PAYMENT_AMOUNT_MISMATCH);
         }
 
         final Payment payment = paymentRepository.findByUserIdAndPostId(user.getId(), postId)
                 .orElseThrow(() -> new GeneralException(TradeException.PAYMENT_NOT_FOUND));
 
         payment.updatePaymentStatus(PaymentStatus.PAID);
+        coinHistoryRepository.save(CoinHistory.of(user, CoinSource.PAYMENT, payment.getUseCoin().intValue()));
+        user.updateUsedCoin(payment.getUseCoin().intValue());
         post.updateIsSold(true);
 
         return GetValidatePaymentResponse.of(payment.getId());
