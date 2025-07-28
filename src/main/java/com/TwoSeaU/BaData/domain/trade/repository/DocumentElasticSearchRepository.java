5e5c8d6354f0cf245e775b9bb20b04c2dc5f988e
Doyeon Kim <wii021004@naver.com>
2025-07-29 00:39:53 +0900

    Fix: delete unused repository

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/DocumentElasticSearchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.TwoSeaU.BaData.domain.trade.repository;
-
-import com.TwoSeaU.BaData.domain.trade.entity.SearchHistoryDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface DocumentElasticSearchRepository extends ElasticsearchRepository<SearchHistoryDocument, String> {
-}
