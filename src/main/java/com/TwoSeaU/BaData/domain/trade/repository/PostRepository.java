c1b7c73bcc6ee99e601ccaa7f562aabaedeedf3b
marineAqu <wii021004@naver.com>
2025-07-30 20:34:15 +0900

    Fix: [BADA-313] 중복 결제 방지를 위한 비관적 락 사용 #320

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
@@ -1,9 +1,15 @@
 package com.TwoSeaU.BaData.domain.trade.repository;
 
 import com.TwoSeaU.BaData.domain.trade.entity.Post;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
     List<Post> findByIsSoldAndIsDeletedOrderByCreatedAtDesc(final boolean isSold, final boolean isDeleted);
@@ -11,4 +17,8 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepo
     List<Post> findByDeadLineBetweenAndIsDeleted(LocalDate start, LocalDate end, boolean isDeleted);
     List<Post> findByIsDeletedAndTitleContaining(final boolean isDeleted, final String query);
     int countBySellerId(final Long sellerId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Post p WHERE p.id = :postId")
+    Optional<Post> findByIdWithLock(@Param("postId") final Long postId);
 }
