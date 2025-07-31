ec4fdff659bfe46abd46c6804531ff777f64c216
choyunju <yoonj1860@naver.com>
2025-07-31 23:29:10 +0900

    Feat: [BADA-328] SOS 요청 시 SSE 기능 구현

--- src/main/java/com/TwoSeaU/BaData/domain/sos/controller/SosController.java
+++ src/main/java/com/TwoSeaU/BaData/domain/sos/controller/SosController.java
@@ -1,16 +1,15 @@
 package com.TwoSeaU.BaData.domain.sos.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.TwoSeaU.BaData.domain.sos.dto.response.RespondSosResponse;
+import com.TwoSeaU.BaData.domain.sos.dto.response.SaveSosResponse;
 import com.TwoSeaU.BaData.domain.sos.service.SosService;
 import com.TwoSeaU.BaData.global.response.ApiResponse;
 
@@ -22,20 +21,14 @@ import lombok.RequiredArgsConstructor;
 public class SosController {
 
 	private final SosService sosService;
-	private final SimpMessagingTemplate template;
 
-	@MessageMapping("/request")
-	public void requestSos() {
-		// sosService.requestSos(user.getUsername());
-
-		template.convertAndSend(
-			"/topic/request",
-			"누군가 데이터 SOS를 요청하였습니다."
-		);
+	@PostMapping("/request")
+	public ResponseEntity<ApiResponse<SaveSosResponse>> requestSos(@AuthenticationPrincipal User user) {
+		return ResponseEntity.ok().body(ApiResponse.success(sosService.requestSos(user.getUsername())));
 	}
 
-	@PostMapping("/{sosId}/respond")
-	public ResponseEntity<ApiResponse<RespondSosResponse>> respondSos(@PathVariable Long sosId, @AuthenticationPrincipal User user) {
+	@PostMapping("/respond")
+	public ResponseEntity<ApiResponse<RespondSosResponse>> respondSos(@RequestParam Long sosId, @AuthenticationPrincipal User user) {
 		return ResponseEntity.ok().body(ApiResponse.success(sosService.respondSos(sosId, user.getUsername())));
 	}
 }
