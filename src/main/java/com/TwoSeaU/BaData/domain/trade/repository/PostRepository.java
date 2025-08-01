e2838bef5df961a5b93e6af4155a8f937a2b821e
marineAqu <wii021004@naver.com>
2025-08-01 22:15:33 +0900

    Refactor: [BADA-338] 미사용 메서드 삭제 #346

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostRepository.java
@@ -7,13 +7,10 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryRepository {
     int countBySellerId(final Long sellerId);
-    List<Post> findTop5ByIsSoldAndIsDeletedOrderByCreatedAtDesc(final boolean isSold, final boolean isDeleted);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT p FROM Post p WHERE p.id = :postId")
