e5a1c7f4e0a8a17463d4793bf10e2a685403d6b7
marineAqu <wii021004@naver.com>
2025-08-02 15:11:25 +0900

    Feat: [BADA-338] 환불 로직 추가 #346

--- src/main/java/com/TwoSeaU/BaData/domain/trade/service/PaymentService.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/service/PaymentService.java
@@ -20,6 +20,7 @@ import com.TwoSeaU.BaData.domain.user.repository.UserRepository;
 import com.TwoSeaU.BaData.global.response.GeneralException;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
+import com.siot.IamportRestClient.request.CancelData;
 import com.siot.IamportRestClient.response.IamportResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -102,6 +103,15 @@ public class PaymentService {
         );
     }
 
+    private void cancelPayment(final String impUid) {
+        try{
+            iamportClient.cancelPaymentByImpUid(new CancelData(impUid, true));
+        }
+        catch (IamportResponseException | IOException e){
+            throw new GeneralException(TradeException.PAYMENT_FAILED);
+        }
+    }
+
     public GetValidatePaymentResponse validateIamport(final String impUid, final Long postId, final String username) throws IamportResponseException, IOException {
         IamportResponse<com.siot.IamportRestClient.response.Payment> portOnePayment = iamportClient.paymentByImpUid(impUid);
 
@@ -116,10 +126,12 @@ public class PaymentService {
                 .orElseThrow(() -> new GeneralException(TradeException.POST_NOT_FOUND));
 
         if (post.getIsDeleted()) {
+            cancelPayment(impUid);
             throw new GeneralException(TradeException.DELETED_POST_ACCESS_DENIED);
         }
 
         if (post.getIsSold()) {
+            cancelPayment(impUid);
             throw new GeneralException(TradeException.PAYMENT_DUPLICATE);
         }
 
@@ -131,10 +143,12 @@ public class PaymentService {
                 .orElseThrow(() -> new GeneralException(TradeException.PAYMENT_NOT_FOUND));
 
         if (payment.getAmount().compareTo(portOnePayment.getResponse().getAmount()) != 0) {
+            cancelPayment(impUid);
             throw new GeneralException(TradeException.PAYMENT_AMOUNT_MISMATCH);
         }
 
         if (payment.getUseCoin().intValue() > user.getCoin()) {
+            cancelPayment(impUid);
             throw new GeneralException(TradeException.COIN_NOT_ENOUGH);
         }
 
