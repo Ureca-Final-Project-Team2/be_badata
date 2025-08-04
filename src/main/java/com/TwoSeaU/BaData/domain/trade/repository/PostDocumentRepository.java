5d415f9aecb447f2b8803c23c85e302fff104d57
marineAqu <wii021004@naver.com>
2025-08-04 09:11:26 +0900

    Fix: [BADA-345] 잘못된 매핑 수정 #359

--- src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostDocumentRepository.java
+++ src/main/java/com/TwoSeaU/BaData/domain/trade/repository/PostDocumentRepository.java
@@ -3,6 +3,6 @@ package com.TwoSeaU.BaData.domain.trade.repository;
 import com.TwoSeaU.BaData.domain.trade.entity.PostDocument;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-public interface PostDocumentRepository extends ElasticsearchRepository<PostDocument, String> {
+public interface PostDocumentRepository extends ElasticsearchRepository<PostDocument, Long> {
 
 }
